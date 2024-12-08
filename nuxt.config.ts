f5ea4b81cee9f712d95444518e4edcb1b1527d30
KoonPaT <40105170+KoonPaT@users.noreply.github.com>
2024-12-08 23:32:26 +0700

    profile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    'bootstrap/dist/css/bootstrap.css',
+    'bootstrap-icons/font/bootstrap-icons.css',
+  ],
+  build: {
+    transpile: ['bootstrap-vue-3'],
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container my-5">
+        <!-- ส่วนหัว -->
+        <div class="text-center mb-5">
+
+        </div>
+
+        <!-- รูปโปรไฟล์ -->
+        <div class="text-center mb-4">
+            <img src="../images/profile.jpg" alt="Profile Picture" class="rounded-circle img-thumbnail"
+                style="width: 200px; height: 200px;" />
+        </div>
+        <div class="text-center mb-5">
+            <h1>PHATTY</h1>
+            Hi! I'm Phat, a fullstack developer who loves gaming, coding, and building cool projects. Connect with
+            me!
+            <p class="lead">
+            </p>
+        </div>
+        <!-- ลิงก์โซเชียล -->
+        <div class="text-center">
+            <a href="https://www.facebook.com/profile.php?id=100008409944091" class="btn btn-primary mx-2"
+                target="_blank">
+                <i class="bi bi-facebook"></i> Facebook
+            </a>
+            <a href="https://www.instagram.com/pat.stp" class="btn btn-danger mx-2" target="_blank">
+                <i class="bi bi-instagram"></i> Instagram
+            </a>
+            <a href="https://github.com/KoonPaT" class="btn btn-dark mx-2" target="_blank">
+                <i class="bi bi-github"></i> GitHub
+            </a>
+            <a href="https://steamcommunity.com/profiles/76561198977544675/" class="btn btn-secondary mx-2"
+                target="_blank">
+                <i class="bi bi-steam"></i> Steam
+            </a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped></style>
